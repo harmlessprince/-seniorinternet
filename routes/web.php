7cc9742045e0784758e39c1466d23c1096277eb2
harmlessprince <realolamilekan@gmail>
2022-01-08 11:33:12 +0100

    welcome page set up

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SemesterController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
@@ -16,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
 
 Route::get('semesters', [SemesterController::class, 'index']);
 Route::get('semesters/{semester}', [SemesterController::class, 'show']);
